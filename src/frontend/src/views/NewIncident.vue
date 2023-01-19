7341b795597cc4ac01bb653f35467631b2ec5f78
Andrespz07 <andresperezortega2001@gmail.com>
2023-01-19 14:16:02 +0100

    vista enlazada, scss instalado y bootstrap enlazado

---
+++ src/frontend/src/views/NewIncident.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+<form>
+  <div class="form-group">
+    
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Enter your name">
+  </div>
+  <div class="form-group">
+    
+    <input type="text" class="form-control form-control-lg" id="formGroupExampleInput2" placeholder="Write a subject">
+  </div>
+
+  <div class="form-group">
+    <textarea class="form-control form-control-lg" id="formGroupExampleInput3" placeholder="Write your commentary" rows="3"></textarea>
+
+  </div>
+</form>
+</template>
+
+<style lang="scss">
+  
+</style>

8abbb73405570d08d97a7db4b9d7db58bc03cb95
NataliaCalvo <natalia.calvo@gmail.com>
2023-01-19 14:23:53 +0100

    finished landing page

--- src/frontend/src/views/LandingPage.vue
+++ src/frontend/src/views/LandingPage.vue
@@ -15,7 +15,7 @@ const props = defineProps ({
   <header id="LandingPage">
     <h1 id="titleLanding">FACTORÍA F5</h1>
     <div id="twoButtons">
-    <button type="buttonHome" id="button1" class="btn btn-light btn-lg">Home</button>
+    <button type="buttonHome" id="button1" class="btn btn-light btn-lg">All Incidents</button>
     <button type="buttonNew" id="button2" class="btn btn-light btn-lg">New Incident</button>
   </div>
   </header>
@@ -27,34 +27,47 @@ const props = defineProps ({
   display: flex;
   flex-direction: column;
   background-image: url(../assets/img/bckground.jpg);
-  background-size:100%;
-  height: 100%;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+  height: 100vh;
+
   
 }
 #titleLanding{
-  font-weight: bolder;
+  font: weight 50em;
   color: white;
-  font-size: xx-large;
+  font-size: 500%;
   align-self: center;
   margin-bottom: 50px;
+  margin-top: 20vh;
 
 }
 #twoButtons{
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  flex-direction: column;
+  margin-left: 38%;
+  width: 25%;
+  flex-wrap: wrap;
+
+
 }
 #button1{
   border: 0.3vh;
   border-style: solid;
   border-color: blue;
   padding:20px;
+  margin-bottom: 75px;
+  font-size:x-large;
+  font-weight: bold;
+
 }
 #button2{
   border: 0.3vh;
   border-style: solid;
   border-color: blue;
   padding:20px;
+  font-size: x-large;
+  font-weight: bold;
 }
 
 </style>

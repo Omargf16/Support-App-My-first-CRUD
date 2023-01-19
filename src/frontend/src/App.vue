7986b55282fa98ac60bee46361206e8d42b85a56
NataliaCalvo <natalia.calvo@gmail.com>
2023-01-19 15:31:14 +0100

    finished footer

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -2,11 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router'
 import LandingPage from './views/LandingPage.vue';
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
 <Header></Header>
 <LandingPage/>
+<Footer></Footer>
 <ButtonsLanding/>
 </template>
 
---
+++ src/frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer id="Footer">
+      <h1 id="titleFooter" class="text-center">© Copyright F5</h1>
+    </footer>
+  </template>
+    
+  <style scoped>
+
+#titleFooter{
+    background-color: blue;
+    font-weight: bold;
+    color: white;
+    font-size: 200%;
+    align-self: center;
+  
+
+}
+  
+  </style>
+  
